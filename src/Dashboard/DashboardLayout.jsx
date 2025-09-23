c30e874be3ed22a0b5a0bb4484af7f12a78bc512
rafika khatun <rafikakhatun607@gmail.com>
2025-09-23 20:22:30 +0530

    Setup React Router with DashboardLayout

--- src/Dashboard/DashboardLayout.jsx
+++ src/Dashboard/DashboardLayout.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+
 
 const DashboardLayout = () => {
     return (
         <div>
+
+            <h1>hello world</h1>
             
         </div>
     );
---
+++ src/Router/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../Dashboard/DashboardLayout";
+
+export const router = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <DashboardLayout></DashboardLayout>
+
+    }
+
+
+
+
+
+
+
+
+
+
+])
